a4d84402182e006f8ef41d686c3e66036c9f85d8
Jerry Vargas <jvargas@conversionia.com>
2020-02-05 10:01:37 -0600

    adding chris' cookies javascript

---
+++ web/scripts/cookie.js
@@ -0,0 +1,29 @@
+// Parse the URL
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// Give the URL parameters variable names
+var r = getParameterByName('r');
+var utm_source = getParameterByName('utm_source');
+var utm_medium = getParameterByName('utm_medium');
+var utm_campaign = getParameterByName('utm_campaign');
+var gclid = getParameterByName('gclid');
+
+// Store
+if(r == null || r == "") {} else { localStorage.setItem("r", r); }
+if(utm_source == null || utm_source == "") {} else { localStorage.setItem("utm_source", utm_source); }
+if(utm_medium == null || utm_medium == "") {} else { localStorage.setItem("utm_medium", utm_medium); }
+if(utm_campaign == null || utm_campaign == "") {} else { localStorage.setItem("utm_campaign", utm_campaign); }
+if(gclid == null || gclid == "") {} else { localStorage.setItem("gclid", gclid); }
+
+// Retrieve
+$("[name='r_param']").val(localStorage.getItem("r"));
+$("[name='utm_source']").val(localStorage.getItem("utm_source"));
+$("[name='utm_medium']").val(localStorage.getItem("utm_medium"));
+$("[name='utm_campaign']").val(localStorage.getItem("utm_campaign"));
+$("[name='gclid']").val(localStorage.getItem("gclid"));
+
