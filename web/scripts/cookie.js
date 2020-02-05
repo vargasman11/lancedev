5564558fe1a728f47a982a0ebf91ca74c8a348ce
Jerry Vargas <jvargas@conversionia.com>
2020-02-05 17:30:51 -0600

    adding LZ's code

--- web/scripts/cookie.js
+++ web/scripts/cookie.js
@@ -1,17 +1,40 @@
-// function addParams() {
-// 	let url = window.location.hostname;
-// 	var query_string = window.location.search;
+// Parse the URL
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
-// 	if (query_string) {
-// 		let elements = [url, query_string]
-// 		elements = elements.join('')
-// 		console.log(elements)
-// 	}
-// }
+// Give the URL parameters variable names
+var r = getParameterByName('r');
+var utm_source = getParameterByName('utm_source');
+var utm_medium = getParameterByName('utm_medium');
+var utm_campaign = getParameterByName('utm_campaign');
+var gclid = getParameterByName('gclid');
 
-// addParams();
+// Store
+if(r == null || r == "") {} else { localStorage.setItem("r", r); }
+if(utm_source == null || utm_source == "") {} else { localStorage.setItem("utm_source", utm_source); }
+if(utm_medium == null || utm_medium == "") {} else { localStorage.setItem("utm_medium", utm_medium); }
+if(utm_campaign == null || utm_campaign == "") {} else { localStorage.setItem("utm_campaign", utm_campaign); }
+if(gclid == null || gclid == "") {} else { localStorage.setItem("gclid", gclid); }
 
-// document.cookie = window.location.search;
-// console.log(document.cookie)
+// Retrieve
+$("[name='r_param']").val(localStorage.getItem("r"));
+$("[name='utm_source']").val(localStorage.getItem("utm_source"));
+$("[name='utm_medium']").val(localStorage.getItem("utm_medium"));
+$("[name='utm_campaign']").val(localStorage.getItem("utm_campaign"));
+$("[name='gclid']").val(localStorage.getItem("gclid"));
+
+let r_param_value = localStorage.getItem("r");
+var utm_source_value = localStorage.getItem("utm_source");
+var utm_medium_value = localStorage.getItem("utm_medium");
+var utm_campaign_value = localStorage.getItem("utm_campaign");
+var utm_gclid_value = localStorage.getItem("glcid");
 
 
+let urlParams = r_param_value + utm_medium_value + utm_campaign + utm_source + gclid;
+
+console.log(window.location.href);
+console.log(urlParams);
